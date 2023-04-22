5d2a622770f34d34f7cf3eb61fd529af4a7a665c
James Njenga <knjenga94@gmail.com>
2023-04-22 21:08:50 +0300

    Add Book seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,22 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # Create sample Stream records
-Stream.create(name: 'Science')
-Stream.create(name: 'Commerce')
-Stream.create(name: 'Arts')
+#Stream.create(name: 'Science')
+#Stream.create(name: 'Commerce')
+#Stream.create(name: 'Arts')
 
 
 # Create sample Student records
-Student.create(name: 'John Doe', email: 'john@example.com', stream: Stream.find_by(name: 'Science'))
-Student.create(name: 'Jane Doe', email: 'jane@example.com', stream: Stream.find_by(name: 'Commerce'))
-Student.create(name: 'Bob Smith', email: 'bob@example.com', stream: Stream.find_by(name: 'Arts'))
+#Student.create(name: 'John Doe', email: 'john@example.com', stream: Stream.find_by(name: 'Science'))
+#Student.create(name: 'Jane Doe', email: 'jane@example.com', stream: Stream.find_by(name: 'Commerce'))
+#Student.create(name: 'Bob Smith', email: 'bob@example.com', stream: Stream.find_by(name: 'Arts'))
+
+# Create book model
+Book.create(title:"You don't know JS", subject:"Javascript",publisher:"JamesN")
+Book.create(title:"Python 101", subject:"Python crash course",publisher:"Nyawira") 
+Book.create(title:"Advanced DSA", subject:"Data Structures",publisher:"E.Juma") 
+Book.create(title:"Backend architecture", subject:"Intro APIs",publisher:"Jay")  
+    
 
 
     
