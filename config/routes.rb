77b7e2d147526aa723f130a1af848271961b959e
James Njenga <knjenga94@gmail.com>
2023-04-22 21:18:29 +0300

    Create books controller and add index route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :students
   resources :streams
 
+  get "/books", to:"books#index"
+
   # display "student_stream#show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
